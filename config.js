4b1a1cd94d2b5d9c7caf62607b51b5bca8cea5ff
Ridz Coder <148397393+ridz-coder01@users.noreply.github.com>
2025-10-03 22:01:53 +0300

    Not for kids

---
+++ config.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) {
+  require('dotenv').config({ path: './config.env' });
+}
+
+function convertToBool(text, fault = 'true') {
+  return text === fault ? true : false;
+}
+
+module.exports = {
+  SESSION_ID: process.env.SESSION_ID || "",
+  MONGODB: process.env.MONGODB || "",
+  
+  // ✅ you can add more config values here
+  PREFIX: process.env.PREFIX || ".",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "263714732501",
+  AUTO_READ: convertToBool(process.env.AUTO_READ, "true"),
+  AUTO_TYPING: convertToBool(process.env.AUTO_TYPING, "true")
+};
